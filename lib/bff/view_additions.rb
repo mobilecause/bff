926423fa7156389c2630beb508818d563b6cc855
Andrew Hunter <hunterae@gmail.com>
2016-07-06 14:04:52 -0600

    Switched old class name BetterFormFor to Bff

--- lib/bff/view_additions.rb
+++ lib/bff/view_additions.rb
@@ -3,7 +3,7 @@ require 'action_view'
 module Bff
   module ViewAdditions
     def bff(record, options={}, &block)
-      BetterFormFor::Builder.new(self, options.merge(record: record)).render_template("bff/form_builder", &block)
+      Bff::Builder.new(self, options.merge(record: record)).render_template("bff/form_builder", &block)
     end
     alias better_form_for bff
   end
